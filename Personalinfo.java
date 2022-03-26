171f0ff10242978183a0bba22b18938a4c286753
Bimidevasia <102397241+Bimidevasia@users.noreply.github.com>
2022-03-26 20:30:10 +0530

    Add files via upload

---
+++ Personalinfo.java
@@ -0,0 +1,18 @@
+class Personalinfo
+{
+	
+	public static void main(String args[])
+  {
+	  int a=3;
+	  int b=a;
+	  System.out.println(b);
+	  System.out.println("Bimi");
+	  System.out.println("Puthuparambil");
+	  System.out.println("Changanacherry");
+	  System.out.println("House No 92");
+	  System.out.println(686105);
+	  
+  }
+  
+
+}
